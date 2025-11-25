4bcc1473105c7f45d57e81369b1a0df374feed87
msaarnela <241745990+msaarnela@users.noreply.github.com>
2025-11-25 14:20:20 +0200

    korjattu apu.js

--- viite-luoja/backend/apu.js
+++ viite-luoja/backend/apu.js
@@ -1,8 +1,12 @@
 import fs from 'fs/promises';
 
+// luodaan absoluuttinen tiedostopolku 
+// (muuten node ei löydä tiedostoa jos komento "node backend.js" on annettu jossain muussa hakemistossa)
+const tiedostopolkuViitemaarittelyt = new URL('./tiedostot/viitemaarittelyt.json', import.meta.url);
+
 export async function haeTiettyTyyppi(viitetyyppi) {
     try {
-      const data = await fs.readFile('./tiedostot/viitemaarittelyt.json', 'utf8');
+      const data = await fs.readFile(tiedostopolkuViitemaarittelyt, 'utf8');
       var jsonData = JSON.parse(data);      
       // jos haluttu viitetyyppi löytyi, palautetaan se
       if (jsonData.hasOwnProperty(viitetyyppi)) {             
@@ -21,7 +25,7 @@ export async function haeKaikkiTyypit() {
     
     // luetaan viitetyyppien määrittelyt
     try {        
-      const data = await fs.readFile('./tiedostot/viitemaarittelyt.json', 'utf8');          
+      const data = await fs.readFile(tiedostopolkuViitemaarittelyt, 'utf8');          
       var jsonData = JSON.parse(data);      
       //Tarkistetaan, että viitetyyppejä on olemassa
       var viitetyypit = Object.keys(jsonData);
