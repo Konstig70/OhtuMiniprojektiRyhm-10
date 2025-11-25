f30a3e22d3e9c7e2c387e1b14f2734f08c43f1dd
Hannes Tornberg <hannes.tornberg@gmail.com>
2025-11-25 18:20:41 +0200

    Fronttia kommentoitu

--- viite-luoja/src/Lomake.jsx
+++ viite-luoja/src/Lomake.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import sendToBackEnd from './App.jsx'
 
 function InputKentat({nimiLista}) {
+  // Poistutaan, jos kenttien nimiä ei annettu
   if (!nimiLista) {
     return
   }
 
+  // Luodaan listaan inputit kaikille kentille
   const InputFields = () => {
     return (
       nimiLista.map(name =>
@@ -17,14 +19,16 @@ function InputKentat({nimiLista}) {
     )
     )}
 
+  // Palautetaan input-kentät
   return (
-    <div>
-      <InputFields />
-    </div>
+    <InputFields />
   )
 }
 
-async function getFields(viitetyyppi, funk) {
+// Muokattu sendToBackEnd-funktio. Ilmeisesti oli tarkoitus muokata kyseistä funktiota ja kaikki backend kutsut
+// lähettää sen kautta, mutta en viitsinyt itsekseni alkaa muokkaamaan ettei mene rikki.
+// Kun sendToBackEnd on muokattu lopulliseen muotoon, tän voi kai poistaa
+async function getFields(viitetyyppi, setNameList) {
   try {
 
     let obj = {"viitetyyppi": viitetyyppi};
@@ -39,9 +43,9 @@ async function getFields(viitetyyppi, funk) {
         throw new Error(`Response status: ${response.status}`)
       }
       
+      // Otetaan vastauksena saadut kentät ja asetetaan ne listaan
       let result = await response.json();
-      funk(result["fields"])
-      console.log(result["fields"]); // debug    
+      setNameList(result["fields"])
       
     } catch (error) {
       console.error(error);
@@ -50,11 +54,15 @@ async function getFields(viitetyyppi, funk) {
 }
 
 function Lomake() {
+  // Lista kenttien nimistä. Oletuksena valittuna viitetyypin article kentät
   const [nameList, setNameList] = useState( [ "author", "title", "year", "journal", "volume", "number", "pages", "doi" ] )
 
   // Tekee pyynnön aina bäckendiin, kun valitsee eri viitetyypin, mikä on ehkä huono
   const handleChange = (e) => {
-    let valittu = e.target.value.toLowerCase()
+    // Dropdown valikosta valittu viitetyyppi muutettuna pieneksi ja tyhjät merkit poistettuna, jotta haku toimii
+    let valittu = e.target.value.toLowerCase().replaceAll(/\s/g,'')
+
+    // Haetaan kentät ja asetetaan listaan, jolloin react tekee taikoja ja kentät tulee näyttöön
     getFields(valittu, setNameList)
   }
 
