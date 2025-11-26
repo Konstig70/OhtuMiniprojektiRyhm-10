d9d67d629c11ec3a44065150cb60cd31e0882a74
emjowebe <emma.j.weber@student.jyu.fi>
2025-11-26 21:07:56 +0200

    muutama testi viitetyyppien kenttien nimistä

---
+++ viite-luoja/src/fields.test.js
@@ -0,0 +1,53 @@
+import { jest } from '@jest/globals';
+
+global.fetch = jest.fn(() => 
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
+
+const {getFields} = await import('./fields.js');
+
+
+describe('getFields', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+    //  tulostaa console.erroriin
+    test('palautetaan null kun ei ole viitetyyppiä', async () => {
+      fetch.mockImplementationOnce(() => Promise.reject(""));
+      const expectedResult = null;
+      const result = await getFields("");
+      expect(result).toEqual(expectedResult);
+      expect(fetch).toHaveBeenCalledWith("http://127.0.0.1:3000/maarittelyt", {
+        method: "POST",
+        body: JSON.stringify({"viitetyyppi": ""}), 
+        headers: { "Content-Type": "application/json" }
+      });
+  });
+
+  test('palautetaan kirjan kentät', async () => {
+    const expectedResult = {"fields": [
+	    "author",
+            "title",
+	    "publisher",
+            "year",
+            "doi"
+    ]};
+    fetch.mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve(expectedResult),	// onko ihan tyhmää?
+      ok: true,
+      status: 200
+    }));
+    const result = await getFields("book");
+    expect(result).toEqual(expectedResult);
+    expect(fetch).toHaveBeenCalledWith("http://127.0.0.1:3000/maarittelyt", {
+      method: "POST",
+      body: JSON.stringify({"viitetyyppi": "book"}), 
+      headers: { "Content-Type": "application/json" }
+      });
+  });
+
+});
+
