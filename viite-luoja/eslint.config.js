b94f0cf8073782817e1ffb48f8ee476540fb17e2
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-12-02 13:03:54 +0200

    eslint configuraatiota muutettu paremmaksi

--- viite-luoja/eslint.config.js
+++ viite-luoja/eslint.config.js
@@ -1,8 +1,14 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import jestPlugin from "eslint-plugin-jest";
 import { defineConfig, globalIgnores } from "eslint/config";
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended, globalIgnores(["dist/"])]);
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, jest: jestPlugin }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.jest} } },
+  pluginReact.configs.flat.recommended, jestPlugin.configs["flat/recommended"], {
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  }, 
+  globalIgnores(["dist/"])]);
