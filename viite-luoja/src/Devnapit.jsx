d4a5ef593a6d6b8bf67a864414d206c5a6fe3908
msaarnela <241745990+msaarnela@users.noreply.github.com>
2025-12-03 20:46:06 +0200

    lisätty latausnapin esimerkki Devnapit.jsx -tiedostoon

--- viite-luoja/src/Devnapit.jsx
+++ viite-luoja/src/Devnapit.jsx
@@ -5,24 +5,31 @@
 // NÄYTETÄÄNKÖ NAPIT VAI EI
 const naytaDevNapit = false;
 
-import { JsonToBibtex } from './funkkarit/jsonToBibtex.jsx';
-
-// ladataan esimerkkidata tiedostosta
-import esimerkkidata from './esimerkkidata.json'
-
 
 export function Devnapit() {
 
   if (naytaDevNapit) {
 
     return (
-      <div id="devnapit">        
-        {esimerkkidata.map(item => <JsonToBibtex jsonObject={item}/>)}        
+      <div id="devnapit">                
+        <DownloadButton textOutput={"rivi 1\nrivi 2\n"} />    
       </div>
     );
 
   }
 
+}
+
+// onnistuu helpoiten linkin download-attribuutilla
+const DownloadButton = ({textOutput}) => {    
+    const file = new Blob([textOutput], {type: 'text/plain'});
+
+    return (            
+            <button className="kopioi"><img src="./src/assets/copy-icon.svg" alt="Lataa tiedosto"/>
+               <a download="viitteet.txt" target="_blank" rel="noreferrer" href={URL.createObjectURL(file)}>Download</a>
+            </button>                                        
+    );
+    
 }
 
 
