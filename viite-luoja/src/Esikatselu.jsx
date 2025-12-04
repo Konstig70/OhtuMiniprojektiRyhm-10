1823b8b7d3281bccaa2452efecdf339ff0cc14fb
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-12-04 10:47:37 +0200

    kuva näkyviin

--- viite-luoja/src/Esikatselu.jsx
+++ viite-luoja/src/Esikatselu.jsx
@@ -43,7 +43,7 @@ function Esikatselu({ viitteet }) {
       <div className="esikatselu">
         <Kentat />
       </div>
-      <button className="kopioi" onClick={kopioiViitteet}><img src="./src/assets/copy-icon.svg" alt="Kopioi leikepöydälle"/></button>
+      <button className="kopioi" onClick={kopioiViitteet}><img src="./viite-luoja/src/assets/copy-icon.svg" alt="Kopioi leikepöydälle"/></button>
     </div>
   )
 }
