35efec4e50ea1d169bc33ce01afeefa898ed00a3
msaarnela <241745990+msaarnela@users.noreply.github.com>
2025-12-04 12:46:42 +0200

    korjattu lintin vaatimukset

--- viite-luoja/src/testit/jsonToBibtexApu.test.js
+++ viite-luoja/src/testit/jsonToBibtexApu.test.js
@@ -1,4 +1,4 @@
-import { jest } from '@jest/globals';
+//import { jest } from '@jest/globals';
 import { jsonToBibtexMuodostaja } from '../funkkarit/jsonToBibtexApu.js';
 
 describe('jsonToBibtexMuodostaja', () => {
