321e1b5c9266f23a6b1bcd0423402801533afd2b
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-12-04 14:14:40 +0200

    testit haeTiettyTyyppi funktiolle

--- viite-luoja/backend/backend.test.js
+++ viite-luoja/backend/backend.test.js
@@ -1,4 +1,6 @@
-import { jest } from '@jest/globals';
+import { describe, jest } from '@jest/globals';
+import { expect } from '@playwright/test';
+//import test from 'node:test';
 
 //Luodaan mockReadFile funktio joka on jest funktio, tässä idea sama kuin pythonin mock luokassa
 const mockReadFile = jest.fn();
@@ -11,7 +13,63 @@ jest.unstable_mockModule('fs/promises', () => ({
 }));
 
 //Importataan backend
-const { haeKaikkiTyypit } = await import('./apu.js');
+const { haeKaikkiTyypit, haeTiettyTyyppi } = await import('./apu.js');
+
+//Testit haeTiettyTyyppi funktiolle 
+describe('haeTiettyTyyppi', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  test('palautetaan JSON muodossa pyydetyt tyypit', async () => {
+    
+    const mockData = {
+            "book":  {"jotain": "muuta"},
+            "article" : {"jotain": "muuta"},
+            "inproceedings" : {"jotain": "muuta"}
+    };
+    
+    const expectedResult = {
+        "jotain": "muuta"
+    };
+    
+    //Nyt siis laitetaan mocReadFile palauttamaan aina mockdata    
+    mockReadFile.mockResolvedValue(JSON.stringify(mockData));
+    
+    //Kutsutaan haeTiettyTyyppi funktiota
+    const result = await haeTiettyTyyppi("book");
+    const tiedostopolkuViitemaarittelyt = new URL('./tiedostot/viitemaarittelyt.json', import.meta.url);
+    //Tulos sama kuin mockdata ja mockReadFile kutsuttu oikealla parametrilla    
+    expect(result).toEqual(expectedResult);
+    expect(mockReadFile).toHaveBeenCalledWith(tiedostopolkuViitemaarittelyt, 'utf8');
+    
+  });
+
+  test('palautetaan null, jos tyyppiä ei löydy', async () => {
+    const mockData = {
+        "book":  {"jotain": "muuta"},
+        "article" : {"jotain": "muuta"},
+        "inproceedings" : {"jotain": "muuta"}
+    };
+    
+    mockReadFile.mockResolvedValue(JSON.stringify(mockData));
+    
+    const result = await haeTiettyTyyppi("Ei löydy");
+    const tiedostopolkuViitemaarittelyt = new URL('./tiedostot/viitemaarittelyt.json', import.meta.url);
+    
+    expect(result).toBeNull();
+    expect(mockReadFile).toHaveBeenCalledWith(tiedostopolkuViitemaarittelyt, 'utf8');
+
+  });
+
+  //Tismalleen sama kuin haekaikkityypit funkkarissa 
+  test('palautetaan null kun tiedoston luku ei onnistu', async () => {
+    mockReadFile.mockRejectedValue(new Error('File not found'));
+    
+    const result = await haeTiettyTyyppi("book");
+    
+    expect(result).toBeNull();
+  });
+
+});
 
 //Testit haeKaikkiTyypit funktiolle. 
 describe('haeKaikkiTyypit', () => {
