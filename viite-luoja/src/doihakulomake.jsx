f1b6ac8c2edee5368ef7ace63f9f2acb718c9d1e
msaarnela <241745990+msaarnela@users.noreply.github.com>
2025-12-11 16:02:09 +0200

    doihakulomake

---
+++ viite-luoja/src/doihakulomake.jsx
@@ -0,0 +1,51 @@
+// 11.12.2025/Micke
+
+import { useState } from 'react'
+import { haeMetadata } from './funkkarit/doihaku.js'
+
+
+export function Doihakulomake() {
+
+    const [doi, setDoi] = useState("");
+    const [doivirhe, setDoivirhe] = useState("");
+
+    function tallennaDoi(e) {        
+        setDoi(e.target.value);        
+        setDoivirhe('');
+    }
+    
+    const haeMetatiedot = async () => {
+        
+        if (doi.trim().length > 0) {
+        
+            setDoivirhe('');
+        
+            const hakutulos = await haeMetadata(doi);
+            
+            if (typeof hakutulos == 'string') {
+                setDoivirhe(hakutulos);
+            }            
+            
+        }       
+                    
+    }
+    
+    return(
+        <div className='inputContainer'>
+            <label>{'DOI: '}
+                <input type="text" className='hakuKentta' onChange={tallennaDoi} defaultValue=''/>
+                <button className="doihaku" onClick={haeMetatiedot}>Hae metatiedot</button>
+            </label>
+            <div>{doivirhe}</div>
+        </div>        
+    );
+
+
+}
+
+
+
+
+
+
+
