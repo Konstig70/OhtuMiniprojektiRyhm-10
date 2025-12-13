2f2a056ea599e62192b363b5de76b350f461bef9
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-12-13 19:25:23 +0200

    poistaminen menee palvelimelle

--- viite-luoja/src/funkkarit/poisto.js
+++ viite-luoja/src/funkkarit/poisto.js
@@ -2,5 +2,23 @@ export function poistaViite(viitteet, setViitteet, poistettava) {
   console.log("poistetaan", poistettava["type"], poistettava["citekey"]); // debug
   let kopio = viitteet.filter(item => item["citekey"] != poistettava["citekey"]);
   setViitteet(kopio);
+  
+  //Konsta 13.12 Poistetaan myös palvelimelta 
+  viePalvelimelle(poistettava);
   return kopio;
 }
+
+async function viePalvelimelle(data) {
+  const response = await fetch("https://ohtuminiprojektiryhm-10-backend.onrender.com/tietokanta/poista", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ viite: data }),
+  });
+  if (!response.ok) {
+      throw new Error(`Virhe lähetyksessä: ${response.status}`);
+    }
+    console.log("Poistaminen onnistu!");
+}
+
